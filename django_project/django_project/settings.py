4cfff8808d0acc67bf9c933a718dabf7144d329a
Neurumaru <kseonjun@yonsei.ac.kr>
2022-11-05 21:11:44 +0000

    SECRET_KEY regenerate

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7nmu7(%#zuth6c%*6s!40vh=95jmi!p99(3iz#qryyfs9f&#^j'
+with open('SECRET_KEY') as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
